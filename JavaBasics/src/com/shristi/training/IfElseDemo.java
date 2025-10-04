cf08d70b17fd6aa4673bd5ce055e91c7a21bc6d1
sachin <sachinsoppin1099@gmail.com>
2025-10-04 19:57:22 +0530

    "Array"

--- JavaBasics/src/com/shristi/training/IfElseDemo.java
+++ JavaBasics/src/com/shristi/training/IfElseDemo.java
@@ -14,7 +14,7 @@ public class IfElseDemo {
 			System.out.println(num1);
 		}
 		else if(num2>num3){
-			System.out.println(num3);
+			System.out.println(num2);
 		}
 		else {
 			System.out.println(num3);
